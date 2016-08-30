88d3955f5e56ce1952fa117c98cc0409e8aad3f1
pasam <pasam@SUSH>
2016-08-30 00:05:26 -0500

    Add project files.

---
+++ OnlineCoursePortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineCoursePortal.MvcApplication" Language="C#" %>
---
+++ OnlineCoursePortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineCoursePortal.Startup))]
+namespace OnlineCoursePortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
